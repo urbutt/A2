81ccbe9d80fd33b4c8e1a77cedf8791db99ab405
urbutt <137639766+urbutt@users.noreply.github.com>
2024-08-26 18:13:23 +0530

    added another problem 456/A

--- Laptops.cpp
+++ Laptops.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+bool alex(vector<pair<int, int>> &arr)
+{
+    int maxValue = arr[0].second;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        if (maxValue > arr[i].second)
+        {
+            return true;
+        }
+        maxValue = max(maxValue, arr[i].second);
+    }
+    return false;
+}
+
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
-    vector<int> arr;
-
+    vector<pair<int, int>> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].first >> arr[i].second;
+    }
+    sort(arr.begin(), arr.end());
+    cout << (alex(arr) ? "Happy Alex" : "Poor Alex");
     return 0;
 }
